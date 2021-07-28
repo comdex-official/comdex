3ec531245c7cf9572483191d84d6ef5dcde43a9f
prashantkrv <prashantkrv96@gmail.com>
2021-07-28 11:03:52 +0530

    updating proto files and protogen script

--- .script/protocgen.sh
+++ .script/protocgen.sh
@@ -45,24 +45,3 @@ Mgoogle/protobuf/any.proto=github.com/cosmos/cosmos-sdk/codec/types:. ./testutil
 # move proto files to the right places
 cp -r github.com/comdex-official/comdex/* ./
 rm -rf github.com
-
-
-##!/usr/bin/env bash
-#
-#set -eo pipefail
-#
-#PROJECT_PROTO_DIR=x/halving/types/
-#COSMOS_SDK_DIR=${COSMOS_SDK_DIR:-$(go list -f "{{ .Dir }}" -m github.com/cosmos/cosmos-sdk)}
-#
-## Generate Go types from protobuf
-#buf protoc \
-#  -I="./proto" \
-#  -I="$COSMOS_SDK_DIR/third_party/proto" \
-#  -I="$COSMOS_SDK_DIR/proto" \
-#  --gocosmos_out=Mgoogle/protobuf/any.proto=github.com/cosmos/cosmos-sdk/codec/types,Mgoogle/protobuf/empty.proto=github.com/gogo/protobuf/types,plugins=interfacetype+grpc,paths=source_relative:. \
-#  --grpc-gateway_out .\
-#  --grpc-gateway_opt logtostderr=true \
-#  --grpc-gateway_opt paths=Mgoogle/protobuf/any.proto=github.com/cosmos/cosmos-sdk/codec/types,Mgoogle/protobuf/empty.proto=github.com/gogo/protobuf/types,paths=source_relative \
-#  --doc_out=./doc \
-#  --doc_opt=markdown,proto.md \
-#  $(find "${PROJECT_PROTO_DIR}" -maxdepth 1 -name '*.proto')
