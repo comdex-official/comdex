49621c0a26d790537401202f1e6a901263df8fa6
prashantkrv2020 <prashant@persistence.one>
2021-07-29 17:59:17 +0530

    suggested changes

--- x/cdp/keeper/msg_server.go
+++ x/cdp/keeper/msg_server.go
@@ -17,32 +17,32 @@ func NewMsgServerImpl(keeper Keeper) types.MsgServer {
 
 var _ types.MsgServer = msgServer{}
 
-func (ms msgServer) CreateCDP(ctx context.Context, msg *types.MsgCreateCDP) (*types.MsgCreateCDPResponse, error) {
+func (ms msgServer) CreateCDP(ctx context.Context, msg *types.MsgCreateCDPRequest) (*types.MsgCreateCDPResponse, error) {
 	//TODO
 	return nil, nil
 }
 
-func (ms msgServer) Deposit(ctx context.Context, msg *types.MsgDeposit) (*types.MsgDepositResponse, error) {
+func (ms msgServer) Deposit(ctx context.Context, msg *types.MsgDepositRequest) (*types.MsgDepositResponse, error) {
 	//TODO
 	return nil, nil
 }
 
-func (ms msgServer) Withdraw(ctx context.Context, msg *types.MsgWithdraw) (*types.MsgWithdrawResponse, error) {
+func (ms msgServer) Withdraw(ctx context.Context, msg *types.MsgWithdrawRequest) (*types.MsgWithdrawResponse, error) {
 	//TODO
 	return nil, nil
 }
 
-func (ms msgServer) DrawDebt(ctx context.Context, msg *types.MsgDrawDebt) (*types.MsgDrawDebtResponse, error) {
+func (ms msgServer) DrawDebt(ctx context.Context, msg *types.MsgDrawDebtRequest) (*types.MsgDrawDebtResponse, error) {
 	//TODO
 	return nil, nil
 }
 
-func (ms msgServer) RepayDebt(ctx context.Context, msg *types.MsgRepayDebt) (*types.MsgRepayDebtResponse, error) {
+func (ms msgServer) RepayDebt(ctx context.Context, msg *types.MsgRepayDebtRequest) (*types.MsgRepayDebtResponse, error) {
 	//TODO
 	return nil, nil
 }
 
-func (ms msgServer) Liquidate(ctx context.Context, msg *types.MsgLiquidate) (*types.MsgLiquidateResponse, error) {
+func (ms msgServer) Liquidate(ctx context.Context, msg *types.MsgLiquidateRequest) (*types.MsgLiquidateResponse, error) {
 	//TODO
 	return nil, nil
 }
