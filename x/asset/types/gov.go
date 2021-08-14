f50124981300ce0ad10d7babe14e7559ba0ad1d1
Srinivas Baride <srini@exidio.co>
2021-08-15 01:55:38 +0530

    Use SDK errors for wrapping instead pkg errors

--- x/asset/types/gov.go
+++ x/asset/types/gov.go
@@ -4,8 +4,8 @@ import (
 	"fmt"
 
 	sdk "github.com/cosmos/cosmos-sdk/types"
+	"github.com/cosmos/cosmos-sdk/types/errors"
 	govtypes "github.com/cosmos/cosmos-sdk/x/gov/types"
-	"github.com/pkg/errors"
 )
 
 const (
@@ -31,22 +31,22 @@ func (m *AddPoolProposal) ValidateBasic() error {
 		return err
 	}
 	if m.DenomIn == "" {
-		return errors.New("denom_in cannot be empty")
+		return fmt.Errorf("denom_in cannot be empty")
 	}
 	if err := sdk.ValidateDenom(m.DenomIn); err != nil {
 		return errors.Wrapf(err, "invalid denom_in %s", m.DenomIn)
 	}
 	if m.DenomOut == "" {
-		return errors.New("denom_out cannot be empty")
+		return fmt.Errorf("denom_out cannot be empty")
 	}
 	if err := sdk.ValidateDenom(m.DenomOut); err != nil {
 		return errors.Wrapf(err, "invalid denom_out %s", m.DenomOut)
 	}
 	if m.LiquidationRatio.IsNil() {
-		return errors.New("liquidation_ratio cannot be nil")
+		return fmt.Errorf("liquidation_ratio cannot be nil")
 	}
 	if m.LiquidationRatio.IsNegative() {
-		return errors.New("liquidation_ratio cannot be negative")
+		return fmt.Errorf("liquidation_ratio cannot be negative")
 	}
 
 	return nil
--- x/asset/types/pool.go
+++ x/asset/types/pool.go
@@ -1,31 +1,33 @@
 package types
 
 import (
+	"fmt"
+
 	sdk "github.com/cosmos/cosmos-sdk/types"
-	"github.com/pkg/errors"
+	"github.com/cosmos/cosmos-sdk/types/errors"
 )
 
 func (m *Pool) Validate() error {
 	if m.Id == 0 {
-		return errors.New("id cannot be zero")
+		return fmt.Errorf("id cannot be zero")
 	}
 	if m.DenomIn == "" {
-		return errors.New("denom_in cannot be empty")
+		return fmt.Errorf("denom_in cannot be empty")
 	}
 	if err := sdk.ValidateDenom(m.DenomIn); err != nil {
 		return errors.Wrapf(err, "invalid denom_in %s", m.DenomIn)
 	}
 	if m.DenomOut == "" {
-		return errors.New("denom_out cannot be empty")
+		return fmt.Errorf("denom_out cannot be empty")
 	}
 	if err := sdk.ValidateDenom(m.DenomOut); err != nil {
 		return errors.Wrapf(err, "invalid denom_out %s", m.DenomOut)
 	}
 	if m.LiquidationRatio.IsNil() {
-		return errors.New("liquidation_ratio cannot be nil")
+		return fmt.Errorf("liquidation_ratio cannot be nil")
 	}
 	if m.LiquidationRatio.IsNegative() {
-		return errors.New("liquidation_ratio cannot be negative")
+		return fmt.Errorf("liquidation_ratio cannot be negative")
 	}
 
 	return nil
