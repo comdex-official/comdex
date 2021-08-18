e9fea1167a77e7d2062aee86683f8bf80451470d
Srinivas Baride <srini@exidio.co>
2021-08-19 03:19:23 +0530

    Added params type

---
+++ x/asset/keeper/params.go
@@ -0,0 +1,32 @@
+package keeper
+
+import (
+	sdk "github.com/cosmos/cosmos-sdk/types"
+
+	"github.com/comdex-official/comdex/x/asset/types"
+)
+
+func (k *Keeper) IBCPort(ctx sdk.Context) (s string) {
+	k.params.Get(ctx, types.KeyIBCPort, &s)
+	return
+}
+
+func (k *Keeper) IBCVersion(ctx sdk.Context) (s string) {
+	k.params.Get(ctx, types.KeyIBCVersion, &s)
+	return
+}
+
+func (k *Keeper) OracleAskCount(ctx sdk.Context) (i uint64) {
+	k.params.Get(ctx, types.KeyOracleAskCount, &i)
+	return
+}
+
+func (k *Keeper) OracleMinCount(ctx sdk.Context) (i uint64) {
+	k.params.Get(ctx, types.KeyOracleMinCount, &i)
+	return
+}
+
+func (k *Keeper) OracleMultiplier(ctx sdk.Context) (i uint64) {
+	k.params.Get(ctx, types.KeyOracleMultiplier, &i)
+	return
+}
