4d98162c3ce81ff294024e8d8ac48300b4db8e5c
Srinivas Baride <srini@exidio.co>
2021-08-25 23:53:47 +0530

    Updated GenesisState with assets, and markets

--- x/asset/types/genesis.go
+++ x/asset/types/genesis.go
@@ -1,14 +1,21 @@
 package types
 
-func NewGenesisState(pairs []Pair, params Params) *GenesisState {
+func NewGenesisState(assets []Asset, markets []Market, pairs []Pair, params Params) *GenesisState {
 	return &GenesisState{
-		Pairs:  pairs,
-		Params: params,
+		Assets:  assets,
+		Markets: markets,
+		Pairs:   pairs,
+		Params:  params,
 	}
 }
 
 func DefaultGenesisState() *GenesisState {
-	return NewGenesisState(nil, Params{})
+	return NewGenesisState(
+		nil,
+		nil,
+		nil,
+		DefaultParams(),
+	)
 }
 
 func ValidateGenesis(_ *GenesisState) error {
