a2b42ce768424b63addb3472519c40b4381bb2fa
Srinivas Baride <srini@exidio.co>
2021-08-27 04:37:53 +0530

    Updated withdraw handler

--- x/cdp/keeper/msg_server.go
+++ x/cdp/keeper/msg_server.go
@@ -121,23 +121,90 @@ func (k *msgServer) MsgDeposit(c context.Context, msg *types.MsgDepositRequest) 
 		return nil, types.ErrorPairDoesNotExist
 	}
 
-	asset, found := k.GetAsset(ctx, pair.AssetIn)
+	assetIn, found := k.GetAsset(ctx, pair.AssetIn)
 	if !found {
 		return nil, types.ErrorAssetDoesNotExist
 	}
 
-	if err := k.SendCoinFromAccountToModule(ctx, from, types.ModuleName, sdk.NewCoin(asset.Denom, msg.Amount)); err != nil {
+	cdp.AmountIn = cdp.AmountIn.Add(msg.Amount)
+	if !cdp.AmountIn.IsPositive() {
+		return nil, types.ErrorInvalidAmount
+	}
+
+	if err := k.SendCoinFromAccountToModule(ctx, from, types.ModuleName, sdk.NewCoin(assetIn.Denom, msg.Amount)); err != nil {
 		return nil, err
 	}
 
-	cdp.AmountIn = cdp.AmountIn.Add(msg.Amount)
 	k.SetCDP(ctx, cdp)
-
 	return &types.MsgDepositResponse{}, nil
 }
 
 func (k *msgServer) MsgWithdraw(c context.Context, msg *types.MsgWithdrawRequest) (*types.MsgWithdrawResponse, error) {
-	panic("implement me")
+	ctx := sdk.UnwrapSDKContext(c)
+
+	from, err := sdk.AccAddressFromBech32(msg.From)
+	if err != nil {
+		return nil, err
+	}
+
+	cdp, found := k.GetCDP(ctx, msg.ID)
+	if !found {
+		return nil, types.ErrorCDPDoesNotExist
+	}
+	if msg.From != cdp.Owner {
+		return nil, types.ErrorUnauthorized
+	}
+
+	pair, found := k.GetPair(ctx, cdp.PairID)
+	if !found {
+		return nil, types.ErrorPairDoesNotExist
+	}
+
+	assetIn, found := k.GetAsset(ctx, pair.AssetIn)
+	if !found {
+		return nil, types.ErrorAssetDoesNotExist
+	}
+
+	assetOut, found := k.GetAsset(ctx, pair.AssetOut)
+	if !found {
+		return nil, types.ErrorAssetDoesNotExist
+	}
+
+	assetInPrice, found := k.GetPriceForAsset(ctx, assetIn.ID)
+	if !found {
+		return nil, types.ErrorPriceDoesNotExist
+	}
+
+	assetOutPrice, found := k.GetPriceForAsset(ctx, assetOut.ID)
+	if !found {
+		return nil, types.ErrorPriceDoesNotExist
+	}
+
+	cdp.AmountIn = cdp.AmountIn.Sub(msg.Amount)
+	if !cdp.AmountIn.IsPositive() {
+		return nil, types.ErrorInvalidAmount
+	}
+
+	totalIn := cdp.AmountIn.Mul(sdk.NewIntFromUint64(assetInPrice)).QuoRaw(assetIn.Decimals).ToDec()
+	if totalIn.IsZero() {
+		return nil, types.ErrorInvalidAmount
+	}
+
+	totalOut := cdp.AmountOut.Mul(sdk.NewIntFromUint64(assetOutPrice)).QuoRaw(assetOut.Decimals).ToDec()
+	if totalOut.IsZero() {
+		return nil, types.ErrorInvalidAmount
+	}
+
+	if totalIn.Quo(totalOut).LT(pair.LiquidationRatio) {
+		return nil, types.ErrorInvalidAmountRatio
+	}
+
+	if err := k.SendCoinFromModuleToAccount(ctx, types.ModuleName, from, sdk.NewCoin(assetIn.Denom, msg.Amount)); err != nil {
+		return nil, err
+	}
+
+	k.SetCDP(ctx, cdp)
+	return &types.MsgWithdrawResponse{}, nil
 }
 
 func (k *msgServer) MsgDraw(c context.Context, msg *types.MsgDrawRequest) (*types.MsgDrawResponse, error) {
