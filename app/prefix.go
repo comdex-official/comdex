c7ff6aa5143c1b2085c4bbce47ab9999c4d38c8f
dheeraj <dheeraj@persistence.one>
2021-09-07 15:40:58 +0530

    Cosmos version upgrade to 44 and address prefix change

---
+++ app/prefix.go
@@ -0,0 +1,23 @@
+package app
+
+import sdk "github.com/cosmos/cosmos-sdk/types"
+
+var (
+	AccountPubKeyPrefix        = AccountAddressPrefix + sdk.PrefixPublic
+	ValidatorAddressPrefix     = AccountAddressPrefix + sdk.PrefixValidator + sdk.PrefixOperator
+	ValidatorPubKeyPrefix      = ValidatorAddressPrefix + sdk.PrefixPublic
+	ConsensusNodeAddressPrefix = AccountAddressPrefix + sdk.PrefixValidator + sdk.PrefixConsensus
+	ConsensusNodePubKeyPrefix  = ConsensusNodeAddressPrefix + sdk.PrefixPublic
+)
+
+// SetAccountAddressPrefixes sets the global prefix to be used when serializing addresses to bech32 strings.
+func SetAccountAddressPrefixes() {
+
+	config := sdk.GetConfig()
+	
+	config.SetBech32PrefixForAccount(AccountAddressPrefix, AccountPubKeyPrefix)
+	config.SetBech32PrefixForValidator(ValidatorAddressPrefix, ValidatorPubKeyPrefix)
+	config.SetBech32PrefixForConsensusNode(ConsensusNodeAddressPrefix, ConsensusNodePubKeyPrefix)
+
+	config.Seal()
+}
--- node/main.go
+++ node/main.go
@@ -3,14 +3,16 @@ package main
 import (
 	"os"
 
-	"github.com/cosmos/cosmos-sdk/server"
-	servercmd "github.com/cosmos/cosmos-sdk/server/cmd"
-
 	comdex "github.com/comdex-official/comdex/app"
 	nodecmd "github.com/comdex-official/comdex/node/cmd"
+	"github.com/cosmos/cosmos-sdk/server"
+	servercmd "github.com/cosmos/cosmos-sdk/server/cmd"
 )
 
 func main() {
+
+	comdex.SetAccountAddressPrefixes()
+
 	root, _ := nodecmd.NewRootCmd()
 	if err := servercmd.Execute(root, comdex.DefaultNodeHome); err != nil {
 		switch e := err.(type) {
