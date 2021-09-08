70499cc2d02cc5812476198bbd24401c791dc762
heis-en-berg <heisenbergknocked@gmail.com>
2021-09-08 19:33:46 -0400

    removed test_helper

--- x/asset/keeper/keeper_test.go
+++ x/asset/keeper/keeper_test.go
@@ -2,6 +2,7 @@ package keeper
 
 import (
 	comdex "github.com/comdex-official/comdex/app"
+	simapp "github.com/comdex-official/comdex/testutil/simapp"
 	sdk "github.com/cosmos/cosmos-sdk/types"
 	"github.com/stretchr/testify/suite"
 	tmproto "github.com/tendermint/tendermint/proto/tendermint/types"
@@ -17,7 +18,7 @@ type KeeperTestSuite struct {
 }
 
 func (suite *KeeperTestSuite) SetupTest() {
-	suite.app = comdex.TestSetup("~/.comdex/testapp")
+	suite.app = simapp.New("~/.comdex/testapp")
 	suite.ctx = suite.app.BaseApp.NewContext(false, tmproto.Header{Height: 1, ChainID: "comdex-test", Time: time.Now().UTC()})
 }
 
