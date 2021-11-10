b6f5c1d87106a33c8aec4ea59c19725bcec68f71
Vishnu Kumavat <43311385+vishnukumavat@users.noreply.github.com>
2021-11-11 00:35:45 +0530

    Swagger Integration (#71)
    * Initial commit for swagger implementation in comdex-official/comdex
    
    * asset module completed
    
    * oracle module added in swagger docs
    
    * vault querier updated
    
    * new query added for vaults
    
    * query minor change reversed
    
    * update proto
    
    * Vault added in swagger json and yaml
    
    Co-authored-by: Vishnu Kumavat <vishnu@Pratiks-Air.lan>
    Co-authored-by: Pratik <pratikasr@gmail.com>
    Co-authored-by: dheeraj <dheeraj@persistence.one>

--- x/vault/client/cli/query.go
+++ x/vault/client/cli/query.go
@@ -22,10 +22,44 @@ func GetQueryCmd() *cobra.Command {
 	}
 
 	cmd.AddCommand(
+		QueryAllVaults(),
 		QueryVault(),
 		QueryVaults(),
 	)
 
+	return cmd
+}
+
+func QueryAllVaults() *cobra.Command {
+	cmd := &cobra.Command{
+		Use:   "vaults",
+		Short: "list of all vaults available",
+		RunE: func(cmd *cobra.Command, args []string) error {
+
+			pagination, err := client.ReadPageRequest(cmd.Flags())
+			if err != nil {
+				return err
+			}
+
+			ctx, err := client.GetClientQueryContext(cmd)
+			if err != nil {
+				return err
+			}
+
+			queryClient := types.NewQueryServiceClient(ctx)
+
+			res, err := queryClient.QueryAllVaults(cmd.Context(), &types.QueryAllVaultsRequest{
+				Pagination: pagination,
+			})
+
+			if err != nil {
+				return err
+			}
+			return ctx.PrintProto(res)
+		},
+	}
+
+	flags.AddQueryFlagsToCmd(cmd)
 	return cmd
 }
 
