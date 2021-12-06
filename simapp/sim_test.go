8de0bd57123deffdb4763c4ebff782c03c07c903
belongstodzire <abhilashxaviers@gmail.com>
2021-12-06 10:14:12 +0530

    simtest changes

--- simapp/sim_test.go
+++ simapp/sim_test.go
@@ -1,31 +1,24 @@
 package simapp
 
 import (
-	"encoding/json"
-	"fmt"
-	"math/rand"
 	"os"
 	"testing"
 
 	app "github.com/comdex-official/comdex/app"
 	"github.com/cosmos/cosmos-sdk/baseapp"
 	sdkSimapp "github.com/cosmos/cosmos-sdk/simapp"
-	"github.com/cosmos/cosmos-sdk/simapp/helpers"
 	"github.com/cosmos/cosmos-sdk/store"
 	simulation2 "github.com/cosmos/cosmos-sdk/types/simulation"
 	"github.com/cosmos/cosmos-sdk/x/simulation"
-	"github.com/stretchr/testify/require"
-	"github.com/tendermint/tendermint/libs/log"
-	dbm "github.com/tendermint/tm-db"
 )
 
 // Profile with:
-// /usr/local/go/bin/go test -benchmem -run=^$ github.com/osmosis-labs/osmosis/simapp -bench ^BenchmarkFullAppSimulation$ -Commit=true -cpuprofile cpu.out
+// /usr/local/go/bin/go test -benchmem -run=^$ github.com/comdex-official/comdex/simapp -bench ^BenchmarkFullAppSimulation$ -Commit=true -cpuprofile cpu.out
 func BenchmarkFullAppSimulation(b *testing.B) {
 	// -Enabled=true -NumBlocks=1000 -BlockSize=200 \
 	// -Period=1 -Commit=true -Seed=57 -v -timeout 24h
 	sdkSimapp.FlagEnabledValue = true
-	sdkSimapp.FlagNumBlocksValue = 1000
+	sdkSimapp.FlagNumBlocksValue = 100
 	sdkSimapp.FlagBlockSizeValue = 200
 	sdkSimapp.FlagCommitValue = true
 	sdkSimapp.FlagVerboseValue = true
@@ -37,8 +30,8 @@ func TestFullAppSimulation(t *testing.T) {
 	// -Enabled=true -NumBlocks=1000 -BlockSize=200 \
 	// -Period=1 -Commit=true -Seed=57 -v -timeout 24h
 	sdkSimapp.FlagEnabledValue = true
-	sdkSimapp.FlagNumBlocksValue = 20
-	sdkSimapp.FlagBlockSizeValue = 25
+	sdkSimapp.FlagNumBlocksValue = 15
+	sdkSimapp.FlagBlockSizeValue = 10
 	sdkSimapp.FlagCommitValue = true
 	sdkSimapp.FlagVerboseValue = true
 	sdkSimapp.FlagPeriodValue = 10
@@ -51,7 +44,6 @@ func fullAppSimulation(tb testing.TB, is_testing bool) {
 	if err != nil {
 		tb.Fatalf("simulation setup failed: %s", err.Error())
 	}
-
 	defer func() {
 		db.Close()
 		err = os.RemoveAll(dir)
@@ -86,7 +78,7 @@ func fullAppSimulation(tb testing.TB, is_testing bool) {
 		tb,
 		os.Stdout,
 		comdex.BaseApp,
-		AppStateFn(comdex.AppCodec(), comdex.SimulationManager()),
+		sdkSimapp.AppStateFn(comdex.AppCodec(), comdex.SimulationManager()),
 		simulation2.RandomAccounts,                                        // Replace with own random account function if using keys other than secp256k1
 		sdkSimapp.SimulationOperations(comdex, comdex.AppCodec(), config), // Run all registered operations
 		comdex.ModuleAccountAddrs(),
@@ -116,77 +108,77 @@ func interBlockCacheOpt() func(*baseapp.BaseApp) {
 
 // // TODO: Make another test for the fuzzer itself, which just has noOp txs
 // // and doesn't depend on the application.
-func TestAppStateDeterminism(t *testing.T) {
-	// if !sdkSimapp.FlagEnabledValue {
-	// 	t.Skip("skipping application simulation")
-	// }
+// func TestAppStateDeterminism(t *testing.T) {
+// 	// if !sdkSimapp.FlagEnabledValue {
+// 	// 	t.Skip("skipping application simulation")
+// 	// }
 
-	config := sdkSimapp.NewConfigFromFlags()
-	config.InitialBlockHeight = 1
-	config.ExportParamsPath = ""
-	config.OnOperation = false
-	config.AllInvariants = false
-	config.ChainID = helpers.SimAppChainID
+// 	config := sdkSimapp.NewConfigFromFlags()
+// 	config.InitialBlockHeight = 1
+// 	config.ExportParamsPath = ""
+// 	config.OnOperation = false
+// 	config.AllInvariants = false
+// 	config.ChainID = helpers.SimAppChainID
 
-	numSeeds := 3
-	numTimesToRunPerSeed := 5
-	appHashList := make([]json.RawMessage, numTimesToRunPerSeed)
+// 	numSeeds := 3
+// 	numTimesToRunPerSeed := 5
+// 	appHashList := make([]json.RawMessage, numTimesToRunPerSeed)
 
-	for i := 0; i < numSeeds; i++ {
-		config.Seed = rand.Int63()
+// 	for i := 0; i < numSeeds; i++ {
+// 		config.Seed = rand.Int63()
 
-		for j := 0; j < numTimesToRunPerSeed; j++ {
-			var logger log.Logger
-			if sdkSimapp.FlagVerboseValue {
-				logger = log.TestingLogger()
-			} else {
-				logger = log.NewNopLogger()
-			}
+// 		for j := 0; j < numTimesToRunPerSeed; j++ {
+// 			var logger log.Logger
+// 			if sdkSimapp.FlagVerboseValue {
+// 				logger = log.TestingLogger()
+// 			} else {
+// 				logger = log.NewNopLogger()
+// 			}
 
-			db := dbm.NewMemDB()
-			app := app.New(
-				logger,
-				db,
-				nil,
-				true,
-				map[int64]bool{},
-				app.DefaultNodeHome,
-				sdkSimapp.FlagPeriodValue,
-				app.MakeEncodingConfig(),
-				sdkSimapp.EmptyAppOptions{},
-				interBlockCacheOpt())
+// 			db := dbm.NewMemDB()
+// 			app := app.New(
+// 				logger,
+// 				db,
+// 				nil,
+// 				true,
+// 				map[int64]bool{},
+// 				app.DefaultNodeHome,
+// 				sdkSimapp.FlagPeriodValue,
+// 				app.MakeEncodingConfig(),
+// 				sdkSimapp.EmptyAppOptions{},
+// 				interBlockCacheOpt())
 
-			fmt.Printf(
-				"Running non-determinism simulation ; seed %d: %d/%d, attempt: %d/%d\n",
-				config.Seed, i+1, numSeeds, j+1, numTimesToRunPerSeed,
-			)
+// 			fmt.Printf(
+// 				"Running non-determinism simulation ; seed %d: %d/%d, attempt: %d/%d\n",
+// 				config.Seed, i+1, numSeeds, j+1, numTimesToRunPerSeed,
+// 			)
 
-			_, _, err := simulation.SimulateFromSeed(
-				t,
-				os.Stdout,
-				app.BaseApp,
-				AppStateFn(app.AppCodec(), app.SimulationManager()),
-				simulation2.RandomAccounts, // Replace with own random account function if using keys other than secp256k1
-				sdkSimapp.SimulationOperations(app, app.AppCodec(), config),
-				app.ModuleAccountAddrs(),
-				config,
-				app.AppCodec(),
-			)
-			require.NoError(t, err)
+// 			_, _, err := simulation.SimulateFromSeed(
+// 				t,
+// 				os.Stdout,
+// 				app.BaseApp,
+// 				AppStateFn(app.AppCodec(), app.SimulationManager()),
+// 				simulation2.RandomAccounts, // Replace with own random account function if using keys other than secp256k1
+// 				sdkSimapp.SimulationOperations(app, app.AppCodec(), config),
+// 				app.ModuleAccountAddrs(),
+// 				config,
+// 				app.AppCodec(),
+// 			)
+// 			require.NoError(t, err)
 
-			if config.Commit {
-				sdkSimapp.PrintStats(db)
-			}
+// 			if config.Commit {
+// 				sdkSimapp.PrintStats(db)
+// 			}
 
-			appHash := app.LastCommitID().Hash
-			appHashList[j] = appHash
+// 			appHash := app.LastCommitID().Hash
+// 			appHashList[j] = appHash
 
-			if j != 0 {
-				require.Equal(
-					t, string(appHashList[0]), string(appHashList[j]),
-					"non-determinism in seed %d: %d/%d, attempt: %d/%d\n", config.Seed, i+1, numSeeds, j+1, numTimesToRunPerSeed,
-				)
-			}
-		}
-	}
-}
+// 			if j != 0 {
+// 				require.Equal(
+// 					t, string(appHashList[0]), string(appHashList[j]),
+// 					"non-determinism in seed %d: %d/%d, attempt: %d/%d\n", config.Seed, i+1, numSeeds, j+1, numTimesToRunPerSeed,
+// 				)
+// 			}
+// 		}
+// 	}
+// }
--- x/vault/simulation/genesis.go
+++ x/vault/simulation/genesis.go
@@ -3,14 +3,7 @@ package simulation
 // DONTCOVER
 
 import (
-	"encoding/json"
-	"fmt"
-	"math/rand"
-
 	"github.com/cosmos/cosmos-sdk/types/module"
-
-	"github.com/comdex-official/comdex/x/vault/types"
-	sdk "github.com/cosmos/cosmos-sdk/types"
 )
 
 // Simulation parameter constants
@@ -20,29 +13,23 @@ const (
 
 // RandomizedGenState generates a random GenesisState for gov
 func RandomizedGenState(simState *module.SimulationState) {
-	// Parameter for how
-	var distrVaultIdentifier string
-	simState.AppParams.GetOrGenerate(
-		simState.Cdc, ParamsDistrVaultIdentifier, &distrVaultIdentifier, simState.Rand,
-		func(r *rand.Rand) { distrVaultIdentifier = GenParamsDistrVaultIdentifier(r) },
-	)
 
-	vaultGenesis := types.GenesisState{
-		Vaults: []types.Vault{
-			{
-				ID:        1,
-				PairID:    1,
-				Owner:     "comdex1hpsnswhtlfu8r5a6psxdszm4p6j98wrj29t6hc",
-				AmountIn:  sdk.Int(sdk.NewInt(100)),
-				AmountOut: sdk.Int(sdk.NewInt(66)),
-			},
-		},
-	}
+	// vaultGenesis := types.GenesisState{
+	// 	// Vaults: []types.Vault{
+	// 	// 	{
+	// 	// 		ID:        1,
+	// 	// 		PairID:    1,
+	// 	// 		Owner:     "comdex1wpd3scc4ulcw3nq3s5q79escym6cccgzm76rxq",
+	// 	// 		AmountIn:  sdk.Int(sdk.NewInt(100)),
+	// 	// 		AmountOut: sdk.Int(sdk.NewInt(66)),
+	// 	// 	},
+	// 	// },
+	// }
 
-	bz, err := json.MarshalIndent(&vaultGenesis, "", " ")
-	if err != nil {
-		panic(err)
-	}
-	fmt.Printf("Selected randomly generated vault parameters:\n%s\n", bz)
-	simState.GenState[types.ModuleName] = simState.Cdc.MustMarshalJSON(&vaultGenesis)
+	// bz, err := json.MarshalIndent(&vaultGenesis, "", " ")
+	// if err != nil {
+	// 	panic(err)
+	// }
+	// fmt.Printf("Selected randomly generated vault parameters:\n%s\n", bz)
+	// simState.GenState[types.ModuleName] = simState.Cdc.MustMarshalJSON(&vaultGenesis)
 }
