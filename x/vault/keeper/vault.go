95818415e13120d78ed17a0b2140d8fe377c0cb6
dheerajkd30 <dheeraj@comdex.one>
2022-01-04 14:10:12 +0530

    modify amount check condition

--- x/vault/keeper/vault.go
+++ x/vault/keeper/vault.go
@@ -162,12 +162,12 @@ func (k *Keeper) CalculateCollaterlizationRatio(
 	}
 
 	totalIn := amountIn.Mul(sdk.NewIntFromUint64(assetInPrice)).QuoRaw(assetIn.Decimals).ToDec()
-	if totalIn.IsPositive() {
+	if totalIn.IsNegative() {
 		return sdk.ZeroDec(), types.ErrorInvalidAmountIn
 	}
 
 	totalOut := amountOut.Mul(sdk.NewIntFromUint64(assetOutPrice)).QuoRaw(assetOut.Decimals).ToDec()
-	if totalOut.IsPositive() {
+	if totalOut.IsNegative() {
 		return sdk.ZeroDec(), types.ErrorInvalidAmountOut
 	}
 
