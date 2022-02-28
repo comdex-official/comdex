0e42253aef9ac1731210c9c2164ad863a0702693
Abhishek Agrawal <abhishek.agrawal@comdex.one>
2022-03-01 01:44:41 +0530

    remove proposal handler for execute contract

--- x/wasm/client/proposal_handler.go
+++ x/wasm/client/proposal_handler.go
@@ -12,10 +12,6 @@ var ProposalHandlers = []govclient.ProposalHandler{
 	govclient.NewProposalHandler(cli.ProposalStoreCodeCmd, rest.StoreCodeProposalHandler),
 	govclient.NewProposalHandler(cli.ProposalInstantiateContractCmd, rest.InstantiateProposalHandler),
 	govclient.NewProposalHandler(cli.ProposalMigrateContractCmd, rest.MigrateProposalHandler),
-	govclient.NewProposalHandler(cli.ProposalExecuteContractCmd, rest.ExecuteProposalHandler),
-	govclient.NewProposalHandler(cli.ProposalSudoContractCmd, rest.SudoProposalHandler),
 	govclient.NewProposalHandler(cli.ProposalUpdateContractAdminCmd, rest.UpdateContractAdminProposalHandler),
 	govclient.NewProposalHandler(cli.ProposalClearContractAdminCmd, rest.ClearContractAdminProposalHandler),
-	govclient.NewProposalHandler(cli.ProposalPinCodesCmd, rest.PinCodeProposalHandler),
-	govclient.NewProposalHandler(cli.ProposalUnpinCodesCmd, rest.UnpinCodeProposalHandler),
 }
