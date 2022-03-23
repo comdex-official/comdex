82ab3797d6a4df8a03b23c6d0e8503625be98d0c
Pratik <pratik@Pratiks-Air.lan>
2022-03-23 17:04:07 +0530

    adding rest tx files

--- x/asset/client/proposal_handler.go
+++ x/asset/client/proposal_handler.go
@@ -2,14 +2,15 @@ package client
 
 import (
 	"github.com/comdex-official/comdex/x/asset/client/cli"
+	"github.com/comdex-official/comdex/x/asset/client/rest"
 	govclient "github.com/cosmos/cosmos-sdk/x/gov/client"
 )
 
 var (
 	AddAssetsHandler = []govclient.ProposalHandler{
-		govclient.NewProposalHandler(cli.NewCmdSubmitAddAssetsProposal, nil),
-		govclient.NewProposalHandler(cli.NewCmdSubmitUpdateAssetProposal, nil),
-		govclient.NewProposalHandler(cli.NewCmdSubmitAddPairsProposal, nil),
-		govclient.NewProposalHandler(cli.NewCmdSubmitUpdatePairProposal, nil),
+		govclient.NewProposalHandler(cli.NewCmdSubmitAddAssetsProposal, rest.AddNewAssetsProposalRESTHandler),
+		govclient.NewProposalHandler(cli.NewCmdSubmitUpdateAssetProposal, rest.UpdateNewAssetProposalRESTHandler),
+		govclient.NewProposalHandler(cli.NewCmdSubmitAddPairsProposal, rest.AddNewPairsProposalRESTHandler),
+		govclient.NewProposalHandler(cli.NewCmdSubmitUpdatePairProposal, rest.UpdateNewPairProposalRESTHandler),
 	}
 )
--- x/bandoracle/client/proposal_handler.go
+++ x/bandoracle/client/proposal_handler.go
@@ -2,9 +2,10 @@ package client
 
 import (
 	"github.com/comdex-official/comdex/x/bandoracle/client/cli"
+	"github.com/comdex-official/comdex/x/bandoracle/client/rest"
 	govclient "github.com/cosmos/cosmos-sdk/x/gov/client"
 )
 
 var (
-	AddFetchPriceHandler = govclient.NewProposalHandler(cli.NewCmdSubmitFetchPriceProposal, nil)
+	AddFetchPriceHandler = govclient.NewProposalHandler(cli.NewCmdSubmitFetchPriceProposal, rest.SubmitFetchPriceProposalRESTHandler)
 )
