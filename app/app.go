3c4e4e019d1c8dde3bcb639ff8ba4c4c19e7a2f6
CryptoVerse <abhishek.agrawal@comdex.one>
2022-05-14 16:03:35 +0530

    change version

--- app/app.go
+++ app/app.go
@@ -802,7 +802,7 @@ func (a *App) ModuleAccountsPermissions() map[string][]string {
 	}
 }
 func (app *App) registerUpgradeHandlers() {
-	app.upgradeKeeper.SetUpgradeHandler("v0.1.2", func(ctx sdk.Context, plan upgradetypes.Plan, _ module.VersionMap) (module.VersionMap, error) {
+	app.upgradeKeeper.SetUpgradeHandler("v0.1.3", func(ctx sdk.Context, plan upgradetypes.Plan, _ module.VersionMap) (module.VersionMap, error) {
 		// 1st-time running in-store migrations, using 1 as fromVersion to
 		// avoid running InitGenesis.
 		fromVM := map[string]uint64{
@@ -840,7 +840,7 @@ func (app *App) registerUpgradeHandlers() {
 	if err != nil {
 		panic(err)
 	}
-	if upgradeInfo.Name == "v0.1.2" && !app.upgradeKeeper.IsSkipHeight(upgradeInfo.Height) {
+	if upgradeInfo.Name == "v0.1.3" && !app.upgradeKeeper.IsSkipHeight(upgradeInfo.Height) {
 		storeUpgrades := storetypes.StoreUpgrades{}
 		// configure store loader that checks if version == upgradeHeight and applies store upgrades
 		app.SetStoreLoader(upgradetypes.UpgradeStoreLoader(upgradeInfo.Height, &storeUpgrades))
