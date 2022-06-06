fab9ad4ab5b15b2a08897fe160bf74ef1e8ea1d7
Chandragupta Singh <chandragupta@MacBook-Air.lan>
2022-06-06 18:03:29 +0530

    rewards updated, vault user end error fixed

--- x/rewards/abci.go
+++ x/rewards/abci.go
@@ -11,19 +11,11 @@ import (
 )
 
 func BeginBlocker(ctx sdk.Context, req abci.RequestBeginBlock, k keeper.Keeper) {
-
 	defer telemetry.ModuleMeasureSince(types.ModuleName, time.Now(), telemetry.MetricKeyBeginBlocker)
 	k.TriggerAndUpdateEpochInfos(ctx)
 
-	rewards := k.GetRewards(ctx)
-	for _, v := range rewards {
-		appId := v.App_mapping_ID
-		assetIds := v.Asset_ID
-		err := k.IterateLocker(ctx, appId, assetIds)
-		if err != nil {
-			return
-		}
-	}
+	k.IterateLocker(ctx)
+
 	AppIdsVault := k.GetAppIds(ctx).WhitelistedAppMappingIdsVaults
 	for i := range AppIdsVault {
 		err := k.IterateVaults(ctx, AppIdsVault[i])
@@ -31,6 +23,7 @@ func BeginBlocker(ctx sdk.Context, req abci.RequestBeginBlock, k keeper.Keeper) 
 			return
 		}
 	}
+
 	err := k.DistributeExtRewardLocker(ctx)
 	if err != nil {
 		return
@@ -39,6 +32,8 @@ func BeginBlocker(ctx sdk.Context, req abci.RequestBeginBlock, k keeper.Keeper) 
 	if err != nil {
 		return
 	}
+
+
 }
 
 // EndBlocker for incentives module.
