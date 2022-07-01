eede1571c38ab3c03d6b49fdfeabab405a4274ce
dheerajkd30 <dheeraj@comdex.one>
2022-07-01 21:16:18 +0530

    resovle test file error

--- x/lend/keeper/grpc_query_params_test.go
+++ x/lend/keeper/grpc_query_params_test.go
@@ -1,21 +1,25 @@
 package keeper_test
 
 import (
+	"github.com/comdex-official/comdex/app"
+	tmproto "github.com/tendermint/tendermint/proto/tendermint/types"
 	"testing"
 
-	testkeeper "github.com/comdex-official/comdex/testutil/keeper"
 	"github.com/comdex-official/comdex/x/lend/types"
 	sdk "github.com/cosmos/cosmos-sdk/types"
 	"github.com/stretchr/testify/require"
 )
 
 func TestParamsQuery(t *testing.T) {
-	keeper, ctx := testkeeper.LendKeeper(t)
+
+	comdexApp := app.Setup(false)
+	ctx := comdexApp.BaseApp.NewContext(false, tmproto.Header{})
+
 	wctx := sdk.WrapSDKContext(ctx)
 	params := types.DefaultParams()
-	keeper.SetParams(ctx, params)
+	comdexApp.LendKeeper.SetParams(ctx, params)
 
-	response, err := keeper.Params(wctx, &types.QueryParamsRequest{})
+	response, err := comdexApp.LendKeeper.Params(wctx, &types.QueryParamsRequest{})
 	require.NoError(t, err)
 	require.Equal(t, &types.QueryParamsResponse{Params: params}, response)
 }
