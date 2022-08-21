8e0ae0f05007ef55ae94093e25534bc141673553
Pratik <pratik@Pratiks-Air.bbrouter>
2022-08-21 16:55:27 +0530

    removing dummy prices

--- x/market/keeper/oracle.go
+++ x/market/keeper/oracle.go
@@ -164,19 +164,14 @@ func (k Keeper) GetMarketForAsset(ctx sdk.Context, id uint64) (market types.Mark
 }
 
 func (k Keeper) GetPriceForAsset(ctx sdk.Context, id uint64) (uint64, bool) {
-	if id != 3 {
-		market, found := k.GetMarketForAsset(ctx, id)
-		if !found {
-			return 0, false
-		}
-
-		rates, found := k.GetPriceForMarket(ctx, market.Symbol)
-		if !found || rates == 0 {
-			return 0, false
-		}
-		return rates, found
-	} else {
-		return 1000000, true
+	market, found := k.GetMarketForAsset(ctx, id)
+	if !found {
+		return 0, false
 	}
 
+	rates, found := k.GetPriceForMarket(ctx, market.Symbol)
+	if !found || rates == 0 {
+		return 0, false
+	}
+	return rates, found
 }
