1140c6ca12e993c439856af71318c572c72192da
Chandragupta Singh <chandragupta@MacBook-Air.local>
2022-08-22 16:00:55 +0530

    genesis export added for vault, rewards

--- x/rewards/genesis.go
+++ x/rewards/genesis.go
@@ -6,19 +6,39 @@ import (
 	sdk "github.com/cosmos/cosmos-sdk/types"
 )
 
-// InitGenesis initializes the capability module's state from a provided genesis
-// state.
-func InitGenesis(ctx sdk.Context, k keeper.Keeper, genState types.GenesisState) {
-	// this line is used by starport scaffolding # genesis/module/init
-	k.SetParams(ctx, genState.Params)
+func InitGenesis(ctx sdk.Context, k keeper.Keeper, state *types.GenesisState) {
+
+	k.SetParams(ctx, state.Params)
+
+	for _, item := range state.InternalRewards {
+		k.SetReward(ctx, item)
+	}
+
+	for _, item := range state.LockerRewardsTracker {
+		k.SetLockerRewardTracker(ctx, item)
+	}
+
+	for _, item := range state.VaultInterestTracker {
+		k.SetVaultInterestTracker(ctx, item)
+		
+	}
+
+	for _, item := range state.LockerExternalRewards {
+		k.SetExternalRewardsLockers(ctx, item)
+	}
+
+	for _, item := range state.VaultExternalRewards {
+		k.SetExternalRewardVault(ctx, item)
+	}
 }
 
-// ExportGenesis returns the capability module's exported genesis.
 func ExportGenesis(ctx sdk.Context, k keeper.Keeper) *types.GenesisState {
-	genesis := types.DefaultGenesis()
-	genesis.Params = k.GetParams(ctx)
-
-	// this line is used by starport scaffolding # genesis/module/export
-
-	return genesis
+	return types.NewGenesisState(
+		k.GetRewards(ctx),
+		k.GetAllLockerRewardTracker(ctx),
+		k.GetAllVaultInterestTracker(ctx),
+		k.GetExternalRewardsLockers(ctx),
+		k.GetExternalRewardVaults(ctx),
+		k.GetParams(ctx),
+	)
 }
