234163429bd5dcc4745201a7f6cba7aa019328aa
Pratik <pratik@Pratiks-Air.bbrouter>
2022-09-06 22:08:20 +0530

    testing upgrade

--- app/upgrades/testnet/v4_0_0/constants.go
+++ app/upgrades/testnet/v4_0_0/constants.go
@@ -29,7 +29,7 @@ const (
 )
 
 const (
-	UpgradeNameV4_2_0   = "v4.2.0"
+	UpgradeNameV4_2_0   = "vtestLend"
 	UpgradeHeightV4_2_0 = "" // replace this height
 	UpgradeInfoV4_2_0   = `'{
 		"binaries": {
