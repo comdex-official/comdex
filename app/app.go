9bb54884d26befd39e1953cc1dbc083b491787e1
Pratik <pratik@Pratiks-Air.bbrouter>
2022-09-30 22:26:04 +0530

    upgrade code added for testing

--- app/app.go
+++ app/app.go
@@ -1142,8 +1142,8 @@ func (a *App) ModuleAccountsPermissions() map[string][]string {
 
 func (a *App) registerUpgradeHandlers() {
 	a.UpgradeKeeper.SetUpgradeHandler(
-		tv4_0_0.UpgradeNameV4_3_0,
-		tv4_0_0.CreateUpgradeHandlerV430(a.mm, a.configurator, a.AssetKeeper),
+		tv4_0_0.UpgradeNameV4_4_0,
+		tv4_0_0.CreateUpgradeHandlerV440(a.mm, a.configurator, a.LendKeeper, a.LiquidationKeeper, a.AuctionKeeper),
 	)
 
 	// When a planned update height is reached, the old binary will panic
@@ -1218,6 +1218,8 @@ func upgradeHandlers(upgradeInfo storetypes.UpgradeInfo, a *App, storeUpgrades *
 	case upgradeInfo.Name == tv4_0_0.UpgradeNameV4_2_0 && !a.UpgradeKeeper.IsSkipHeight(upgradeInfo.Height):
 		storeUpgrades = &storetypes.StoreUpgrades{}
 	case upgradeInfo.Name == tv4_0_0.UpgradeNameV4_3_0 && !a.UpgradeKeeper.IsSkipHeight(upgradeInfo.Height):
+		storeUpgrades = &storetypes.StoreUpgrades{}
+	case upgradeInfo.Name == tv4_0_0.UpgradeNameV4_4_0 && !a.UpgradeKeeper.IsSkipHeight(upgradeInfo.Height):
 		storeUpgrades = &storetypes.StoreUpgrades{}
 	}
 	return storeUpgrades
