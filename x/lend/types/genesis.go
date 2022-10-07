020989b05eef599a48b1a3c9e77364222f34f464
Chandragupta Singh <chandragupta@MacBook-Air.bbrouter>
2022-10-08 04:54:26 +0530

    genesis commented for temp

--- x/lend/types/genesis.go
+++ x/lend/types/genesis.go
@@ -1,55 +1,55 @@
 package types
 
-// func NewGenesisState(borrowAsset []BorrowAsset, userBorrowIDMapping []UserBorrowIdMapping, borrowIDByOwnerAndPoolMapping []BorrowIdByOwnerAndPoolMapping, borrowMapping BorrowMapping, lendAsset []LendAsset, pool []Pool, assetToPairMapping []AssetToPairMapping, userLendIdMapping []UserLendIdMapping, lendIdByOwnerAndPoolMapping []LendIdByOwnerAndPoolMapping, lendIdToBorrowIdMapping []LendIdToBorrowIdMapping, assetStats []AssetStats, lendMapping LendMapping, userdepositStats DepositStats, reservedepositStats DepositStats, buybackdepositStats DepositStats, borrowdepositStats DepositStats, extended_Pair []Extended_Pair, assetRatesStats []AssetRatesStats, auctionParams []AuctionParams, params Params) *GenesisState {
-// 	return &GenesisState{
-// 		BorrowAsset:                   borrowAsset,
-// 		UserBorrowIdMapping:           userBorrowIDMapping,
-// 		BorrowIdByOwnerAndPoolMapping: borrowIDByOwnerAndPoolMapping,
-// 		BorrowMapping:                 borrowMapping,
-// 		LendAsset:                     lendAsset,
-// 		Pool:                          pool,
-// 		AssetToPairMapping:            assetToPairMapping,
-// 		UserLendIdMapping:             userLendIdMapping,
-// 		LendIdByOwnerAndPoolMapping:   lendIdByOwnerAndPoolMapping,
-// 		LendIdToBorrowIdMapping:       lendIdToBorrowIdMapping,
-// 		AssetStats:                    assetStats,
-// 		LendMapping:                   lendMapping,
-// 		UserDepositStats:              userdepositStats,
-// 		ReserveDepositStats:           reservedepositStats,
-// 		BuyBackDepositStats:           buybackdepositStats,
-// 		BorrowDepositStats:            borrowdepositStats,
-// 		Extended_Pair:                 extended_Pair,
-// 		AssetRatesStats:               assetRatesStats,
-// 		AuctionParams:                 auctionParams,
-// 		Params:                        params,
-// 	}
-// }
+func NewGenesisState() *GenesisState {
+	return &GenesisState{
+		// BorrowAsset:                   borrowAsset,
+		// UserBorrowIdMapping:           userBorrowIDMapping,
+		// BorrowIdByOwnerAndPoolMapping: borrowIDByOwnerAndPoolMapping,
+		// BorrowMapping:                 borrowMapping,
+		// LendAsset:                     lendAsset,
+		// Pool:                          pool,
+		// AssetToPairMapping:            assetToPairMapping,
+		// UserLendIdMapping:             userLendIdMapping,
+		// LendIdByOwnerAndPoolMapping:   lendIdByOwnerAndPoolMapping,
+		// LendIdToBorrowIdMapping:       lendIdToBorrowIdMapping,
+		// AssetStats:                    assetStats,
+		// LendMapping:                   lendMapping,
+		// UserDepositStats:              userdepositStats,
+		// ReserveDepositStats:           reservedepositStats,
+		// BuyBackDepositStats:           buybackdepositStats,
+		// BorrowDepositStats:            borrowdepositStats,
+		// Extended_Pair:                 extended_Pair,
+		// AssetRatesStats:               assetRatesStats,
+		// AuctionParams:                 auctionParams,
+		// Params:                        params,
+	}
+}
 
-// func DefaultGenesisState() *GenesisState {
-// 	return NewGenesisState(
-// 		[]BorrowAsset{},
-// 		[]UserBorrowIdMapping{},
-// 		[]BorrowIdByOwnerAndPoolMapping{},
-// 		BorrowMapping{},
-// 		[]LendAsset{},
-// 		[]Pool{},
-// 		[]AssetToPairMapping{},
-// 		[]UserLendIdMapping{},
-// 		[]LendIdByOwnerAndPoolMapping{},
-// 		[]LendIdToBorrowIdMapping{},
-// 		[]AssetStats{},
-// 		LendMapping{},
-// 		DepositStats{},
-// 		DepositStats{},
-// 		DepositStats{},
-// 		DepositStats{},
-// 		[]Extended_Pair{},
-// 		[]AssetRatesStats{},
-// 		[]AuctionParams{},
-// 		DefaultParams(),
-// 	)
-// }
+func DefaultGenesisState() *GenesisState {
+	return NewGenesisState(
+		// []BorrowAsset{},
+		// []UserBorrowIdMapping{},
+		// []BorrowIdByOwnerAndPoolMapping{},
+		// BorrowMapping{},
+		// []LendAsset{},
+		// []Pool{},
+		// []AssetToPairMapping{},
+		// []UserLendIdMapping{},
+		// []LendIdByOwnerAndPoolMapping{},
+		// []LendIdToBorrowIdMapping{},
+		// []AssetStats{},
+		// LendMapping{},
+		// DepositStats{},
+		// DepositStats{},
+		// DepositStats{},
+		// DepositStats{},
+		// []Extended_Pair{},
+		// []AssetRatesStats{},
+		// []AuctionParams{},
+		// DefaultParams(),
+	)
+}
 
-// func (m *GenesisState) Validate() error {
-// 	return nil
-// }
+func (m *GenesisState) Validate() error {
+	return nil
+}
