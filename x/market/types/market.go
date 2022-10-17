a99d00442fab66c548bb78b44984acbfec38b911
Chandragupta Singh <chandragupta@MacBook-Air.local>
2022-10-17 10:03:49 +0530

    redundant check removed

--- x/market/types/market.go
+++ x/market/types/market.go
@@ -9,9 +9,6 @@ const (
 )
 
 func (m *TimeWeightedAverage) Validate() error {
-	if m.AssetID < 0 {
-		return fmt.Errorf("id cannot be less than zero")
-	}
 	if m.ScriptID == 0 {
 		return fmt.Errorf("script_id cannot be zero")
 	}
