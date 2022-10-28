f6af262d453930dbfd7fb592972b5efbc1bd206a
Pratik <pratikasr@gmail.com>
2022-10-28 21:56:15 +0530

    adding -1

--- app/upgrades/testnet/v5_0_0/upgrades.go
+++ app/upgrades/testnet/v5_0_0/upgrades.go
@@ -1,6 +1,11 @@
 package v5_0_0 //nolint:revive,stylecheck
 
 import (
+	"fmt"
+	"github.com/comdex-official/comdex/x/asset"
+	assettypes "github.com/comdex-official/comdex/x/asset/types"
+	"github.com/comdex-official/comdex/x/lend"
+	lendtypes "github.com/comdex-official/comdex/x/lend/types"
 	"github.com/cosmos/cosmos-sdk/codec"
 	sdk "github.com/cosmos/cosmos-sdk/types"
 	"github.com/cosmos/cosmos-sdk/types/module"
@@ -14,8 +19,10 @@ func CreateUpgradeHandlerV5Beta(
 ) upgradetypes.UpgradeHandler {
 	return func(ctx sdk.Context, _ upgradetypes.Plan, fromVM module.VersionMap) (module.VersionMap, error) {
 		// This change is only for testnet upgrade
-		//fromVM[lendtypes.ModuleName] = lend.AppModule{}.ConsensusVersion()
-		//fromVM[assettypes.ModuleName] = asset.AppModule{}.ConsensusVersion()
+		fromVM[lendtypes.ModuleName] = lend.AppModule{}.ConsensusVersion() - 1
+		fromVM[assettypes.ModuleName] = asset.AppModule{}.ConsensusVersion() - 1
+		fmt.Println("fromVM[lendtypes.ModuleName]", fromVM[lendtypes.ModuleName])
+		fmt.Println("fromVM[assettypes.ModuleName]", fromVM[assettypes.ModuleName])
 		//mm.Modules[lendtypes.ModuleName].InitGenesis(ctx, cdc, myCustomGenesisState)
 		return mm.RunMigrations(ctx, configurator, fromVM)
 	}
