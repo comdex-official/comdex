ea7f8c00d9cc861ef753ca6ab323ce0a14071e95
Chandragupta Singh <chandragupta.singh4@gmail.com>
2022-11-02 14:27:45 +0530

    asset migration added

--- x/asset/migrations/v5/migrations.go
+++ x/asset/migrations/v5/migrations.go
@@ -1,22 +1,26 @@
 package v5
 
 import (
-	"fmt"
 	assettypes "github.com/comdex-official/comdex/x/asset/types"
 	"github.com/cosmos/cosmos-sdk/codec"
 	storetypes "github.com/cosmos/cosmos-sdk/store/types"
 	sdk "github.com/cosmos/cosmos-sdk/types"
-	protobuftypes "github.com/gogo/protobuf/types"
 )
 
 func MigrateStore(ctx sdk.Context, storeKey storetypes.StoreKey, cdc codec.BinaryCodec) error {
 	store := ctx.KVStore(storeKey)
-	fmt.Println("MigrateStore")
-	return MigrateValueApps(store, cdc)
+	err := MigrateValueApps(store, cdc)
+	if err != nil {
+		return err
+	}
+	err = MigrateValueAsset(store, cdc)
+	if err != nil {
+		return err
+	}
+	return err
 }
 
 func MigrateValueApps(store sdk.KVStore, cdc codec.BinaryCodec) error {
-
 	app1 := assettypes.AppData{
 		Id:               1,
 		Name:             "CSWAP",
@@ -60,7 +64,142 @@ func MigrateValueApps(store sdk.KVStore, cdc codec.BinaryCodec) error {
 	key3 := assettypes.AppKey(app3.Id)
 	store.Delete(key3)
 	SetApp(store, cdc, app3)
-	SetAppID(store, cdc, 3)
+	return nil
+}
+
+func MigrateValueAsset(store sdk.KVStore, cdc codec.BinaryCodec) error {
+	asset1 := assettypes.Asset{
+		Id:                    1,
+		Name:                  "ATOM",
+		Denom:                 "ibc/2E5D0AC026AC1AFA65A23023BA4F24BB8DDF94F118EDC0BAD6F625BFC557CDED",
+		Decimals:              sdk.NewInt(1000000),
+		IsOnChain:             false,
+		IsOraclePriceRequired: true,
+	}
+	key1 := assettypes.AssetKey(asset1.Id)
+	store.Delete(key1)
+	SetAsset(store, cdc, asset1)
+
+	asset2 := assettypes.Asset{
+		Id:                    2,
+		Name:                  "CMDX",
+		Denom:                 "ucmdx",
+		Decimals:              sdk.NewInt(1000000),
+		IsOnChain:             false,
+		IsOraclePriceRequired: true,
+	}
+	key2 := assettypes.AssetKey(asset2.Id)
+	store.Delete(key2)
+	SetAsset(store, cdc, asset2)
+
+	asset3 := assettypes.Asset{
+		Id:                    3,
+		Name:                  "CMST",
+		Denom:                 "ucmst",
+		Decimals:              sdk.NewInt(1000000),
+		IsOnChain:             false,
+		IsOraclePriceRequired: false,
+	}
+	key3 := assettypes.AssetKey(asset3.Id)
+	store.Delete(key3)
+	SetAsset(store, cdc, asset3)
+
+	asset4 := assettypes.Asset{
+		Id:                    4,
+		Name:                  "OSMO",
+		Denom:                 "ibc/868AF0A32D53849B6093348F5A47BB969A98E71A3F0CD2D3BE406EA25DA7F836",
+		Decimals:              sdk.NewInt(1000000),
+		IsOnChain:             false,
+		IsOraclePriceRequired: true,
+	}
+	key4 := assettypes.AssetKey(asset4.Id)
+	store.Delete(key4)
+	SetAsset(store, cdc, asset4)
+
+	asset5 := assettypes.Asset{
+		Id:                    5,
+		Name:                  "cATOM",
+		Denom:                 "ucatom",
+		Decimals:              sdk.NewInt(1000000),
+		IsOnChain:             false,
+		IsOraclePriceRequired: false,
+	}
+	key5 := assettypes.AssetKey(asset5.Id)
+	store.Delete(key5)
+	SetAsset(store, cdc, asset5)
+
+	asset6 := assettypes.Asset{
+		Id:                    6,
+		Name:                  "cCMDX",
+		Denom:                 "uccmdx",
+		Decimals:              sdk.NewInt(1000000),
+		IsOnChain:             false,
+		IsOraclePriceRequired: false,
+	}
+	key6 := assettypes.AssetKey(asset6.Id)
+	store.Delete(key6)
+	SetAsset(store, cdc, asset6)
+
+	asset7 := assettypes.Asset{
+		Id:                    7,
+		Name:                  "cCMST",
+		Denom:                 "uccmst",
+		Decimals:              sdk.NewInt(1000000),
+		IsOnChain:             false,
+		IsOraclePriceRequired: false,
+	}
+	key7 := assettypes.AssetKey(asset7.Id)
+	store.Delete(key7)
+	SetAsset(store, cdc, asset7)
+
+	asset8 := assettypes.Asset{
+		Id:                    8,
+		Name:                  "cOSMO",
+		Denom:                 "ucosmo",
+		Decimals:              sdk.NewInt(1000000),
+		IsOnChain:             false,
+		IsOraclePriceRequired: false,
+	}
+	key8 := assettypes.AssetKey(asset8.Id)
+	store.Delete(key8)
+	SetAsset(store, cdc, asset8)
+
+	asset9 := assettypes.Asset{
+		Id:                    9,
+		Name:                  "HARBOR",
+		Denom:                 "uharbor",
+		Decimals:              sdk.NewInt(1000000),
+		IsOnChain:             true,
+		IsOraclePriceRequired: false,
+	}
+	key9 := assettypes.AssetKey(asset9.Id)
+	store.Delete(key9)
+	SetAsset(store, cdc, asset9)
+
+	asset10 := assettypes.Asset{
+		Id:                    10,
+		Name:                  "USDC",
+		Denom:                 "ibc/EF8A76D0FD3F3F45D8DB7FEBFCF921206DF58CA41493ED16D69BF7B4E061C60C",
+		Decimals:              sdk.NewInt(1000000),
+		IsOnChain:             false,
+		IsOraclePriceRequired: true,
+	}
+	key10 := assettypes.AssetKey(asset10.Id)
+	store.Delete(key10)
+	SetAsset(store, cdc, asset10)
+
+	asset11 := assettypes.Asset{
+		Id:                    11,
+		Name:                  "WETH",
+		Denom:                 "ibc/A99459944FD67B5711735B4B4D3FE30BA45328E94D437C78E47CA8DEFA781E49",
+		Decimals:              sdk.NewInt(1000000000000000000),
+		IsOnChain:             false,
+		IsOraclePriceRequired: true,
+	}
+	key11 := assettypes.AssetKey(asset11.Id)
+	store.Delete(key11)
+	SetAsset(store, cdc, asset11)
+
 	return nil
 }
 
@@ -73,14 +212,10 @@ func SetApp(store sdk.KVStore, cdc codec.BinaryCodec, app assettypes.AppData) {
 	store.Set(key, value)
 }
 
-func SetAppID(store sdk.KVStore, cdc codec.BinaryCodec, id uint64) {
+func SetAsset(store sdk.KVStore, cdc codec.BinaryCodec, asset assettypes.Asset) {
 	var (
-		key   = assettypes.AppIDKey
-		value = cdc.MustMarshal(
-			&protobuftypes.UInt64Value{
-				Value: id,
-			},
-		)
+		key   = assettypes.AssetKey(asset.Id)
+		value = cdc.MustMarshal(&asset)
 	)
 
 	store.Set(key, value)
