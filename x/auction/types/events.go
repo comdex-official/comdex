57ce464c48d77b019bf38b7ffe44c3c3c4195993
Dheeraj Dubey <dheeraj@comdex.one>
2022-11-08 16:15:11 +0530

    add event emmiters in auction abci

---
+++ x/auction/types/events.go
@@ -0,0 +1,18 @@
+package types
+
+const (
+	SurplusActivator               = "surplus_activator"
+	DebtActivator                  = "debt_activator"
+	RestartDutch                   = "restart_dutch"
+	RestartLendDutch               = "restart_lend_dutch"
+	DataAppID                      = "data_app_id"
+	DataAssetID                    = "data_asset_id"
+	DataAssetOutOraclePrice        = "data_asset_out_oracle_price"
+	DataAssetOutPrice              = "data_asset_out_price"
+	DatIsAuctionActive             = "data_is_auction_active"
+	DataIsDebtAuction              = "data_is_debt_auction"
+	DataIsDistributor              = "data_is_distributor"
+	DataIsSurplusAuction           = "data_is_surplus_auction"
+	KillSwitchParamsBreakerEnabled = "data_is_surplus_auction"
+	Status                         = "status"
+)
