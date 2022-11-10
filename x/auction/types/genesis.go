ec23b00e14e1a5c2eca8f3c51b2c4f5916e8ead2
Chandragupta Singh <chandragupta.singh4@gmail.com>
2022-11-11 00:08:32 +0530

    genesis corrected

--- x/auction/types/genesis.go
+++ x/auction/types/genesis.go
@@ -1,12 +1,13 @@
 package types
 
-func NewGenesisState(surplusAuction []SurplusAuction, debtAuction []DebtAuction, dutchAuction []DutchAuction, protocolStatistics []ProtocolStatistics, auctionParams []AuctionParams, params Params, userBiddingID uint64) *GenesisState {
+func NewGenesisState(surplusAuction []SurplusAuction, debtAuction []DebtAuction, dutchAuction []DutchAuction, protocolStatistics []ProtocolStatistics, auctionParams []AuctionParams, dutchLendAuction []DutchAuction, params Params, userBiddingID uint64) *GenesisState {
 	return &GenesisState{
 		SurplusAuction:     surplusAuction,
 		DebtAuction:        debtAuction,
 		DutchAuction:       dutchAuction,
 		ProtocolStatistics: protocolStatistics,
 		AuctionParams:      auctionParams,
+		DutchLendAuction:   dutchLendAuction,
 		Params:             params,
 		UserBiddingID:      userBiddingID,
 	}
@@ -20,6 +21,7 @@ func DefaultGenesisState() *GenesisState {
 		[]DutchAuction{},
 		[]ProtocolStatistics{},
 		[]AuctionParams{},
+		[]DutchAuction{},
 		DefaultParams(),
 		UserBiddingID,
 	)
--- x/vault/types/genesis.go
+++ x/vault/types/genesis.go
@@ -1,20 +1,23 @@
 package types
 
-func NewGenesisState(vaults []Vault, stableMintVault []StableMintVault, appExtendedPairVaultMapping []AppExtendedPairVaultMappingData, userVaultAssetMapping []OwnerAppExtendedPairVaultMappingData) *GenesisState {
+func NewGenesisState(vaults []Vault, stableMintVault []StableMintVault, appExtendedPairVaultMapping []AppExtendedPairVaultMappingData, userVaultAssetMapping []OwnerAppExtendedPairVaultMappingData, lengthOfvaults uint64) *GenesisState {
 	return &GenesisState{
 		Vaults:                      vaults,
 		StableMintVault:             stableMintVault,
 		AppExtendedPairVaultMapping: appExtendedPairVaultMapping,
 		UserVaultAssetMapping:       userVaultAssetMapping,
+		LengthOfVaults:              lengthOfvaults,
 	}
 }
 
 func DefaultGenesisState() *GenesisState {
+	var length uint64
 	return NewGenesisState(
 		[]Vault{},
 		[]StableMintVault{},
 		[]AppExtendedPairVaultMappingData{},
 		[]OwnerAppExtendedPairVaultMappingData{},
+		length,
 	)
 }
 
