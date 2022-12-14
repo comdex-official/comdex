bd78989b60652b439927ba17110b3280730f6115
Pratik <pratikasr@gmail.com>
2022-12-14 11:00:04 +0530

    genesis setter fn

--- x/lend/genesis.go
+++ x/lend/genesis.go
@@ -72,6 +72,11 @@ func InitGenesis(ctx sdk.Context, k keeper.Keeper, state *types.GenesisState) {
 	k.SetUserLendIDCounter(ctx, lendID)
 	k.SetPoolID(ctx, poolID)
 	k.SetLendPairID(ctx, extendedPairID)
+	k.SetFundModBal(ctx, state.ModBal)
+	k.SetFundReserveBal(ctx, state.ReserveBal)
+	for _, item := range state.AllReserveStats {
+		k.SetAllReserveStatsByAssetID(ctx, item)
+	}
 }
 
 func ExportGenesis(ctx sdk.Context, k keeper.Keeper) *types.GenesisState {
