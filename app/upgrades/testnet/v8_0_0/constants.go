f1f3a3c16acc5cd883d12d897e281854617f29ab
Pratik <pratikasr@gmail.com>
2023-02-07 05:34:50 +0530

    upgrade code for increasing consenus version of lend module & rewards module

---
+++ app/upgrades/testnet/v8_0_0/constants.go
@@ -0,0 +1,15 @@
+package v8_0_0 //nolint:revive,stylecheck
+
+const (
+	UpgradeName820Beta = "v8.2.0.beta"
+	UpgradeHeight      = ""
+	UpgradeInfo        = `'{
+		"binaries": {
+			"darwin/arm64":"",
+			"darwin/x86_64":"",
+			"linux/arm64":"",
+			"linux/x86_64":"",
+			"windows/x86_64":""
+		}
+	}'`
+)
