82f78900485e084ebef82afca93f4c4162c0b12a
Rohit Rasjurya Prasad <42783861+rajrohit10@users.noreply.github.com>
2023-04-07 23:55:03 +0530

    liquidation-liquidateVaults

--- x/liquidationsV2/keeper/liquidate.go
+++ x/liquidationsV2/keeper/liquidate.go
@@ -4,9 +4,6 @@ import (
 	"fmt"
 
 	utils "github.com/comdex-official/comdex/types"
-	assettypes "github.com/comdex-official/comdex/x/asset/types"
-	auctiontypes "github.com/comdex-official/comdex/x/auction/types"
-	lendtypes "github.com/comdex-official/comdex/x/lend/types"
 	"github.com/comdex-official/comdex/x/liquidationsV2/types"
 	rewardstypes "github.com/comdex-official/comdex/x/rewards/types"
 	sdk "github.com/cosmos/cosmos-sdk/types"
@@ -53,60 +50,12 @@ func (k Keeper) LiquidateVaults(ctx sdk.Context) error {
 	for _, vault := range newVaults {
 		_ = utils.ApplyFuncIfNoError(ctx, func(ctx sdk.Context) error {
 
-			//Checking ESM status and / or kill switch status
-			esmStatus, found := k.esm.GetESMStatus(ctx, vault.AppId)
-			klwsParams, _ := k.esm.GetKillSwitchData(ctx, vault.AppId)
-			if (found && esmStatus.Status) || klwsParams.BreakerEnable {
-				ctx.Logger().Error("Kill Switch Or ESM is enabled For Liquidation, liquidate_vaults.go for AppID %d", vault.AppId)
-				continue
-			}
-
-			//Checking if app has enabled liquidations or not
-			_, found = k.GetAppIDByAppForLiquidation(ctx, vault.AppId)
-
-			if !found {
-				return fmt.Errorf("Liquidation not enabled for App ID  %d", vault.AppId)
-			}
-
-			// Checking extended pair vault data for Minimum collateralisation ratio
-			extPair, _ := k.asset.GetPairsVault(ctx, vault.ExtendedPairVaultID)
-			liqRatio := extPair.MinCr
-			totalOut := vault.AmountOut.Add(vault.InterestAccumulated).Add(vault.ClosingFeeAccumulated)
-			collateralizationRatio, err := k.vault.CalculateCollateralizationRatio(ctx, vault.ExtendedPairVaultID, vault.AmountIn, totalOut)
+			err := k.LiquidateIndividualVault(ctx, vault.Id)
 			if err != nil {
-				return fmt.Errorf("error Calculating CR in Liquidation, liquidate_vaults.go for vault ID %d", vault.Id)
+
+				return fmt.Errorf(err.Error())
 			}
-			if collateralizationRatio.LT(liqRatio) {
-				totalDebt := vault.AmountOut.Add(vault.InterestAccumulated)
-				err1 := k.rewards.CalculateVaultInterest(ctx, vault.AppId, vault.ExtendedPairVaultID, vault.Id, totalDebt, vault.BlockHeight, vault.BlockTime.Unix())
-				if err1 != nil {
-					return fmt.Errorf("error Calculating vault interest in Liquidation, liquidate_vaults.go for vaultID %d", vault.Id)
-				}
-				//Callling vault to use the updated values of the vault
-				vault, _ := k.vault.GetVault(ctx, vault.Id)
 
-				totalOut := vault.AmountOut.Add(vault.InterestAccumulated).Add(vault.ClosingFeeAccumulated)
-				collateralizationRatio, err := k.vault.CalculateCollateralizationRatio(ctx, vault.ExtendedPairVaultID, vault.AmountIn, totalOut)
-				if err != nil {
-					return fmt.Errorf("error Calculating CR in Liquidation, liquidate_vaults.go for vaultID %d", vault.Id)
-				}
-
-				//Creating locked vault struct , which will trigger auction
-				err = k.CreateLockedVault(ctx, vault.Id, vault.ExtendedPairVaultID, vault.Owner, vault.AmountIn, totalOut, collateralizationRatio, vault.AppId, false, false, "", "")
-				if err != nil {
-					return fmt.Errorf("error Creating Locked Vaults in Liquidation, liquidate_vaults.go for Vault %d", vault.Id)
-				}
-				length := k.vault.GetLengthOfVault(ctx)
-				k.vault.SetLengthOfVault(ctx, length-1)
-
-				//Removing data from existing structs
-				k.vault.DeleteVault(ctx, vault.Id)
-				var rewards rewardstypes.VaultInterestTracker
-				rewards.AppMappingId = vault.AppId
-				rewards.VaultId = vault.Id
-				k.rewards.DeleteVaultInterestTracker(ctx, rewards)
-				k.vault.DeleteAddressFromAppExtendedPairVaultMapping(ctx, vault.ExtendedPairVaultID, vault.Id, vault.AppId)
-			}
 			return nil
 		})
 	}
@@ -116,6 +65,99 @@ func (k Keeper) LiquidateVaults(ctx sdk.Context) error {
 
 	return nil
 
+}
+
+func (k Keeper) LiquidateIndividualVault(ctx sdk.Context, vaultID uint64) error {
+
+	vault, found := k.vault.GetVault(ctx, vaultID)
+	if !found {
+		return fmt.Errorf("Vault ID not found  %d", vault.Id)
+	}
+
+	//Checking ESM status and / or kill switch status
+	esmStatus, found := k.esm.GetESMStatus(ctx, vault.AppId)
+	klwsParams, _ := k.esm.GetKillSwitchData(ctx, vault.AppId)
+	if (found && esmStatus.Status) || klwsParams.BreakerEnable {
+		return fmt.Errorf("Kill Switch Or ESM is enabled For Liquidation,")
+	}
+
+	//Checking if app has enabled liquidations or not
+	_, found = k.GetAppIDByAppForLiquidation(ctx, vault.AppId)
+	if !found {
+		return fmt.Errorf("Liquidation not enabled for App ID  %d", vault.AppId)
+	}
+
+	// Checking extended pair vault data for Minimum collateralisation ratio
+	extPair, _ := k.asset.GetPairsVault(ctx, vault.ExtendedPairVaultID)
+	liqRatio := extPair.MinCr
+	totalOut := vault.AmountOut.Add(vault.InterestAccumulated).Add(vault.ClosingFeeAccumulated)
+	collateralizationRatio, err := k.vault.CalculateCollateralizationRatio(ctx, vault.ExtendedPairVaultID, vault.AmountIn, totalOut)
+	if err != nil {
+		return fmt.Errorf("error Calculating CR in Liquidation, liquidate_vaults.go for vault ID %d", vault.Id)
+	}
+	if collateralizationRatio.LT(liqRatio) {
+		totalDebt := vault.AmountOut.Add(vault.InterestAccumulated)
+		err1 := k.rewards.CalculateVaultInterest(ctx, vault.AppId, vault.ExtendedPairVaultID, vault.Id, totalDebt, vault.BlockHeight, vault.BlockTime.Unix())
+		if err1 != nil {
+			return fmt.Errorf("error Calculating vault interest in Liquidation, liquidate_vaults.go for vaultID %d", vault.Id)
+		}
+		//Callling vault to use the updated values of the vault
+		vault, _ := k.vault.GetVault(ctx, vault.Id)
+
+		totalOut := vault.AmountOut.Add(vault.InterestAccumulated).Add(vault.ClosingFeeAccumulated)
+		collateralizationRatio, err := k.vault.CalculateCollateralizationRatio(ctx, vault.ExtendedPairVaultID, vault.AmountIn, totalOut)
+		if err != nil {
+			return fmt.Errorf("error Calculating CR in Liquidation, liquidate_vaults.go for vaultID %d", vault.Id)
+		}
+		//Calculating Liquidation Fees
+		feesToBeCollected := sdk.NewDecFromInt(totalOut).Mul(extPair.LiquidationPenalty).TruncateInt()
+
+		//Creating locked vault struct , which will trigger auction
+		err = k.CreateLockedVault(ctx, vault.Id, vault.ExtendedPairVaultID, vault.Owner, vault.AmountIn, totalOut, collateralizationRatio, vault.AppId, false, false, "", "", feesToBeCollected)
+		if err != nil {
+			return fmt.Errorf("error Creating Locked Vaults in Liquidation, liquidate_vaults.go for Vault %d", vault.Id)
+		}
+		length := k.vault.GetLengthOfVault(ctx)
+		k.vault.SetLengthOfVault(ctx, length-1)
+
+		//Removing data from existing structs
+		k.vault.DeleteVault(ctx, vault.Id)
+		var rewards rewardstypes.VaultInterestTracker
+		rewards.AppMappingId = vault.AppId
+		rewards.VaultId = vault.Id
+		k.rewards.DeleteVaultInterestTracker(ctx, rewards)
+		k.vault.DeleteAddressFromAppExtendedPairVaultMapping(ctx, vault.ExtendedPairVaultID, vault.Id, vault.AppId)
+	}
+
+	return nil
+}
+
+func (k Keeper) CreateLockedVault(ctx sdk.Context, OriginalVaultId, ExtendedPairId uint64, Owner string, AmountIn sdk.Int, AmountOut sdk.Int, collateralizationRatio sdk.Dec, appID uint64, isInternalKeeper bool, isExternalKeeper bool, internalKeeperAddress string, externalKeeperAddress string, feesToBeCollected sdk.Int) error {
+	lockedVaultID := k.GetLockedVaultID(ctx)
+
+	value := types.LockedVault{
+		LockedVaultId:                lockedVaultID + 1,
+		AppId:                        appID,
+		OriginalVaultId:              OriginalVaultId,
+		ExtendedPairId:               ExtendedPairId,
+		Owner:                        Owner,
+		AmountIn:                     AmountIn,
+		AmountOut:                    AmountOut,
+		CurrentCollaterlisationRatio: collateralizationRatio,
+		CollateralToBeAuctioned:      AmountIn,
+		TargetDebt:                   AmountOut,
+		LiquidationTimestamp:         ctx.BlockTime(),
+		FeeToBeCollected:             feesToBeCollected,
+		IsInternalKeeper:             false,
+		InternalKeeperAddress:        "",
+		IsExternalKeeper:             "",
+		ExternalKeeperAddress:        "",
+	}
+
+	k.SetLockedVault(ctx, value)
+	k.SetLockedVaultID(ctx, value.LockedVaultId)
+
+	return nil
 }
 
 func (k Keeper) LiquidateBorrows(ctx sdk.Context) error {
@@ -322,35 +364,6 @@ func (k Keeper) UpdateLockedBorrows(ctx sdk.Context, updatedLockedVault types.Lo
 	updatedLockedVault.CollateralToBeAuctioned = selloffAmount
 	k.SetLockedVault(ctx, updatedLockedVault)
 	k.SetLockedVaultID(ctx, updatedLockedVault.LockedVaultId)
-
-	return nil
-}
-
-func (k Keeper) CreateLockedVault(ctx sdk.Context, OriginalVaultId, ExtendedPairId uint64, Owner string, AmountIn sdk.Int, AmountOut sdk.Int, collateralizationRatio sdk.Dec, appID uint64, isInternalKeeper bool, isExternalKeeper bool, internalKeeperAddress string, externalKeeperAddress string) error {
-	lockedVaultID := k.GetLockedVaultID(ctx)
-
-	value := types.LockedVault{
-		LockedVaultId:                lockedVaultID + 1,
-		AppId:                        appID,
-		OriginalVaultId:              OriginalVaultId,
-		ExtendedPairId:               ExtendedPairId,
-		Owner:                        Owner,
-		AmountIn:                     AmountIn,
-		AmountOut:                    AmountOut,
-		CurrentCollaterlisationRatio: collateralizationRatio,
-		CollateralToBeAuctioned:      AmountIn,
-		TargetDebt:                   AmountOut,
-		LiquidationTimestamp:         ctx.BlockTime(),
-		FeeToBeCollected: ,
-		IsInternalKeeper:             false,
-		InternalKeeperAddress:        "",
-		IsExternalKeeper:             "",
-		ExternalKeeperAddress:        "",
-	
-	}
-
-	k.SetLockedVault(ctx, value)
-	k.SetLockedVaultID(ctx, value.LockedVaultId)
 
 	return nil
 }
