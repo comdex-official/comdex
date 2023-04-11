4be820e1c1c7e4e8d3ebae0852ea2c7498193414
Pratik <pratikasr@gmail.com>
2023-04-11 17:30:53 +0530

    updating expected keepers

--- x/auctionsV2/keeper/auctions.go
+++ x/auctionsV2/keeper/auctions.go
@@ -1,25 +1,15 @@
 package keeper
 
-import(
-	
+import (
+
 	// "github.com/comdex-official/comdex/types"
 	liquidationtypes "github.com/comdex-official/comdex/x/liquidationsV2/types"
 	sdk "github.com/cosmos/cosmos-sdk/types"
-
 )
 
+func (k Keeper) AuctionActivator(ctx sdk.Context, liquidationData liquidationtypes.LockedVault) error {
 
+	auctionType, _ := k.liquidationsV2.GetLiquidationWhiteListing(ctx, liquidationData.AppId)
 
-func (k Keeper) AuctionActivator(ctx sdk.Context,lockedVault liquidationtypes.LockedVault) error {
-
-	//Using app id provided in the lockedVault Struct
-	// Using the app id to fetch the app data whitelisted in the liquidation module to find the auction type selected by the app.
-	appWhitelistedData:=k.liquidation.GetLiquidationWhiteListing(ctx,lockedVault.AppId)
-
-	
-
-
-
-	
 	return nil
 }
--- x/auctionsV2/keeper/keeper.go
+++ x/auctionsV2/keeper/keeper.go
@@ -4,8 +4,15 @@ import (
 	"context"
 	"fmt"
 
+<<<<<<< HEAD
 	"github.com/comdex-official/comdex/x/liquidationsV2/expected"
 	"github.com/comdex-official/comdex/x/liquidationsV2/types"
+=======
+	"github.com/tendermint/tendermint/libs/log"
+
+	"github.com/comdex-official/comdex/x/auctionsV2/expected"
+	"github.com/comdex-official/comdex/x/auctionsV2/types"
+>>>>>>> 6cc24557 (updating expected keepers)
 	"github.com/cosmos/cosmos-sdk/codec"
 	sdk "github.com/cosmos/cosmos-sdk/types"
 	paramtypes "github.com/cosmos/cosmos-sdk/x/params/types"
@@ -14,12 +21,21 @@ import (
 
 type (
 	Keeper struct {
+<<<<<<< HEAD
 		cdc         codec.BinaryCodec
 		storeKey    sdk.StoreKey
 		memKey      sdk.StoreKey
 		paramstore  paramtypes.Subspace
 		liquidation expected.LiquidationV2Keeper
 		bankKeeper  types.BankKeeper
+=======
+		cdc            codec.BinaryCodec
+		storeKey       sdk.StoreKey
+		memKey         sdk.StoreKey
+		paramstore     paramtypes.Subspace
+		bankKeeper     types.BankKeeper
+		liquidationsV2 expected.LiquidationsV2Keeper
+>>>>>>> 6cc24557 (updating expected keepers)
 	}
 )
 
@@ -33,8 +49,12 @@ func NewKeeper(
 	storeKey,
 	memKey sdk.StoreKey,
 	ps paramtypes.Subspace,
+<<<<<<< HEAD
 	liquidation expected.LiquidationV2Keeper,
+=======
+>>>>>>> 6cc24557 (updating expected keepers)
 	bankKeeper types.BankKeeper,
+	liquidationsV2Keeper expected.LiquidationsV2Keeper,
 ) Keeper {
 	// set KeyTable if it has not already been set
 	if !ps.HasKeyTable() {
@@ -43,12 +63,21 @@ func NewKeeper(
 
 	return Keeper{
 
+<<<<<<< HEAD
 		cdc:        cdc,
 		storeKey:   storeKey,
 		memKey:     memKey,
 		paramstore: ps,
 		liquidation: liquidation,
 		bankKeeper: bankKeeper,
+=======
+		cdc:            cdc,
+		storeKey:       storeKey,
+		memKey:         memKey,
+		paramstore:     ps,
+		bankKeeper:     bankKeeper,
+		liquidationsV2: liquidationsV2Keeper,
+>>>>>>> 6cc24557 (updating expected keepers)
 	}
 }
 
