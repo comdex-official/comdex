3c085c5f1a8c174bbe3a016abf47bfaad2f2fac5
Pratik <pratikasr@gmail.com>
2023-04-11 17:34:14 +0530

    updating expected keepers

--- x/auctionsV2/keeper/keeper.go
+++ x/auctionsV2/keeper/keeper.go
@@ -3,39 +3,23 @@ package keeper
 import (
 	"context"
 	"fmt"
-
-<<<<<<< HEAD
-	"github.com/comdex-official/comdex/x/liquidationsV2/expected"
-	"github.com/comdex-official/comdex/x/liquidationsV2/types"
-=======
 	"github.com/tendermint/tendermint/libs/log"
 
 	"github.com/comdex-official/comdex/x/auctionsV2/expected"
 	"github.com/comdex-official/comdex/x/auctionsV2/types"
->>>>>>> 6cc24557 (updating expected keepers)
 	"github.com/cosmos/cosmos-sdk/codec"
 	sdk "github.com/cosmos/cosmos-sdk/types"
 	paramtypes "github.com/cosmos/cosmos-sdk/x/params/types"
-	"github.com/tendermint/tendermint/libs/log"
 )
 
 type (
 	Keeper struct {
-<<<<<<< HEAD
-		cdc         codec.BinaryCodec
-		storeKey    sdk.StoreKey
-		memKey      sdk.StoreKey
-		paramstore  paramtypes.Subspace
-		liquidation expected.LiquidationV2Keeper
-		bankKeeper  types.BankKeeper
-=======
 		cdc            codec.BinaryCodec
 		storeKey       sdk.StoreKey
 		memKey         sdk.StoreKey
 		paramstore     paramtypes.Subspace
 		bankKeeper     types.BankKeeper
 		liquidationsV2 expected.LiquidationsV2Keeper
->>>>>>> 6cc24557 (updating expected keepers)
 	}
 )
 
@@ -49,10 +33,6 @@ func NewKeeper(
 	storeKey,
 	memKey sdk.StoreKey,
 	ps paramtypes.Subspace,
-<<<<<<< HEAD
-	liquidation expected.LiquidationV2Keeper,
-=======
->>>>>>> 6cc24557 (updating expected keepers)
 	bankKeeper types.BankKeeper,
 	liquidationsV2Keeper expected.LiquidationsV2Keeper,
 ) Keeper {
@@ -62,22 +42,12 @@ func NewKeeper(
 	}
 
 	return Keeper{
-
-<<<<<<< HEAD
-		cdc:        cdc,
-		storeKey:   storeKey,
-		memKey:     memKey,
-		paramstore: ps,
-		liquidation: liquidation,
-		bankKeeper: bankKeeper,
-=======
 		cdc:            cdc,
 		storeKey:       storeKey,
 		memKey:         memKey,
 		paramstore:     ps,
 		bankKeeper:     bankKeeper,
 		liquidationsV2: liquidationsV2Keeper,
->>>>>>> 6cc24557 (updating expected keepers)
 	}
 }
 
