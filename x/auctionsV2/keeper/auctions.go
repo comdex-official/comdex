5919256d322c895dabe167311cc2844b94b9b78a
Pratik <pratikasr@gmail.com>
2023-04-16 00:27:15 +0530

    updating pb files

--- x/auctionsV2/keeper/auctions.go
+++ x/auctionsV2/keeper/auctions.go
@@ -28,6 +28,7 @@ func (k Keeper) AuctionActivator(ctx sdk.Context, liquidationData liquidationtyp
 }
 
 func (k Keeper) DutchAuctionActivator(ctx sdk.Context, liquidationData liquidationtypes.LockedVault) error {
-
+	// define opening price and closing price
+	// set data in generic auction params
 	return nil
 }
