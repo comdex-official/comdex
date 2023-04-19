d81992c34028d31a522554890be175cd7feac5fc
Dheeraj Dubey <dheeraj@comdex.one>
2023-04-19 17:31:37 +0530

    test net v0.10.1 upgrade handler

---
+++ app/upgrades/testnet/v10/constants.go
@@ -0,0 +1,15 @@
+package v10
+
+const (
+	UpgradeName   = "v10.1.0"
+	UpgradeHeight = ""
+	UpgradeInfo   = `'{
+		"binaries": {
+			"darwin/arm64":"",
+			"darwin/x86_64":"",
+			"linux/arm64":"",
+			"linux/x86_64":"",
+			"windows/x86_64":""
+		}
+	}'`
+)
