2d41a166f73ab8d112371c22f5b13e49a595a6f6
Pratik <pratikasr@gmail.com>
2023-08-08 20:52:48 +0530

    updating module.go

--- x/auctionsV2/module.go
+++ x/auctionsV2/module.go
@@ -1,6 +1,7 @@
 package auctionsV2
 
 import (
+	"context"
 	"encoding/json"
 	"fmt"
 	// this line is used by starport scaffolding # 1
@@ -73,8 +74,8 @@ func (AppModuleBasic) RegisterRESTRoutes(clientCtx client.Context, rtr *mux.Rout
 }
 
 // RegisterGRPCGatewayRoutes registers the gRPC Gateway routes for the module.
-func (AppModuleBasic) RegisterGRPCGatewayRoutes(clientCtx client.Context, mux *runtime.ServeMux) {
-	// this line is used by starport scaffolding # 2
+func (AppModuleBasic) RegisterGRPCGatewayRoutes(ctx client.Context, mux *runtime.ServeMux) {
+	_ = types.RegisterQueryHandlerClient(context.Background(), mux, types.NewQueryClient(ctx))
 }
 
 // GetTxCmd returns the capability module's root tx command.
