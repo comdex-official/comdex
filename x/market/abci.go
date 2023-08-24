c552023115c2c0e360841cfda444d6cbe7cc927c
Chandragupta Singh <chandragupta.singh4@gmail.com>
2023-08-24 11:20:45 +0530

    abci fmts for testing

--- x/market/abci.go
+++ x/market/abci.go
@@ -1,6 +1,8 @@
 package market
 
 import (
+	"fmt"
+
 	assetkeeper "github.com/comdex-official/comdex/x/asset/keeper"
 	bandkeeper "github.com/comdex-official/comdex/x/bandoracle/keeper"
 	bandoraclemoduletypes "github.com/comdex-official/comdex/x/bandoracle/types"
@@ -60,5 +62,10 @@ func BeginBlocker(ctx sdk.Context, _ abci.RequestBeginBlock, k keeper.Keeper, ba
 			k.SetTwa(ctx, twa)
 		}
 	}
+	fmt.Println("----------+++++++++++--------------")
+	fmt.Println("---------")
+	fmt.Println(k.GetRandomSeed(ctx, ctx.BlockHeight()))
+	fmt.Println("---------")
+	fmt.Println("----------+++++++++++--------------")
 	
 }
