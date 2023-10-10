5278e23267dca8989d55325b244c8a675cb60b9e
vishnukumavat <vishnukumawat0950@gmail.com>
2023-10-11 04:59:26 +0530

    integration test suite for liquidity refactored

--- x/liquidity/client/testutil/cli_test.go
+++ x/liquidity/client/testutil/cli_test.go
@@ -1,11 +1,5 @@
 package testutil
 
-import (
-	"testing"
-
-	"github.com/stretchr/testify/suite"
-)
-
-func TestLiquidityIntegrationTestSuite(t *testing.T) {
-	suite.Run(t, new(LiquidityIntegrationTestSuite))
-}
+// func TestLiquidityIntegrationTestSuite(t *testing.T) {
+// 	suite.Run(t, new(LiquidityIntegrationTestSuite))
+// }
