39c98d93cd55d6f792883f7744c7b423a530aa4b
Pratik <pratikasr@gmail.com>
2024-02-20 16:43:40 +0530

    adding upgrade handlers

---
+++ app/upgrades/mainnet/v14/constants.go
@@ -0,0 +1,15 @@
+package v14
+
+const (
+	UpgradeName   = "v14.0.0"
+	UpgradeHeight = ""
+	UpgradeInfo   = `'{
+		"binaries": {
+			"darwin/arm64":"",
+			"darwin/x86_64":"",
+			"linux/arm64":"",
+			"linux/x86_64":"",
+			"windows/x86_64":""
+		}
+	}'`
+)
